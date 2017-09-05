84124fc0700f1fc7e81979bfb5fd224b7dcc9332
Rien Croonenborghs <rien@croonenborghs.net>
2017-09-05 17:32:06 +1200

    request

--- app/slack/request.rb
+++ app/slack/request.rb
@@ -2,44 +2,41 @@ module Slack
   class Request
     attr_accessor :response_url
     
-    HELP      = "help"
-    SEARCH    = "search"
-    FOLLOW    = "follow"
-    UNFOLLOW  = "unfollow"
-
     def initialize params
-      @user = Slack::User.new params[:user_id], params[:user_name]
-      # actions for (un)following tv programs
-      @payload  = JSON.parse params[:payload] || "{}"
-      @actions  = @payload["actions"] || []
-      # command
-      parsed_text = (params[:text] || "").split(" ")
-      @command    = parsed_text.shift
-      @options    = parsed_text
-
-      @response_url = params[:response_url] || @payload["response_url"]
+      @response_url = nil
+      @actions_request = ActionsRequest.new params
+      @command_request = CommandRequest.new params
     end
 
     # /tv help
     # /tv search
     # /tv list
     def process
-      if actions?
-        process_actions
-      elsif help?
-        process_help
-      elsif search?
-        process_search      
-      else
-        process_help
-      end
+      @actions_request.process
+      @command_request.process
+    end
+  end # class Request
+
+  class CommandRequest < Request
+    def initialize(params)
+      split_text    = (params[:text] || "").split(" ")
+      @command      = split_text.shift
+      @options      = split_text
+      @response_url ||= params[:response_url]
+    end
+
+    def process
+      return process_help   if help?
+      return process_search if search?
+      process_help
     end
 
   private
 
     def help?
-      @command == HELP || @command.nil?
+      @command == "help"
     end
+
     def process_help
       help = ["`/tv help` helps you along the way"]
       help << "`/tv search tv program` finds out things about *tv program*"
@@ -48,10 +45,11 @@ module Slack
     end
 
     def search?
-      @command == SEARCH
+      @command == "search"
     end
+
     def process_search
-      return Slack::Response::ToYouOnly.text "Nothing to search for. Try something like `/tv search game of thrones`." unless @options.any?
+      return Slack::Response::ToYouOnly.text "Nothing to search for." unless @options.any?
       begin
         api     = TMDb::API::Search.new @options.join(" ")
         results = api.search
@@ -60,19 +58,35 @@ module Slack
       rescue => e
         Slack::Response::ToYouOnly.error e
       end
+    end    
+  end
+
+  class ActionsRequest < Request
+    def initialize(params)
+      @payload      = JSON.parse params[:payload] || "{}"
+      @actions      = @payload["actions"] || []
+      @response_url ||= @payload["response_url"]
     end
+
+    def process
+      return process_actions if actions?
+    end
+
+  private
 
     def actions?
       @actions.any?
     end
+
     def process_actions
       begin
         attachments = [].tap do |ret|
           @actions.each do |action|
-            ret << process_follow(action)   if action["name"] == FOLLOW
-            ret << process_unfollow(action) if action["name"] == UNFOLLOW
+            puts "action: #{action['name']} - #{action['value']}"
+            ret << process_follow(action)   if action["name"] == "follow"
+            ret << process_unfollow(action) if action["name"] == "unfollow"
           end
-        end.join("\n")
+        end.flatten
         
         Slack::Response::ToYouOnly.attachments do
           attachments
@@ -81,18 +95,25 @@ module Slack
         Slack::Response::ToYouOnly.error e
       end
     end
+
     def process_follow action
-      api     = TMDb::API::TvShows.new
-      tv_show = api.get action["value"]
-      {
-        fallback:     "You have started following #{tv_show[:name]}.",
-        color:        "#36a64f", # greenish
-        title:        "You have started following #{tv_show[:name]}.",
-        text:         tv_show[:name],
-      }
-    end
-    def process_unfollow action
+      puts "process_follow"
+      begin
+        api     = TMDb::API::TvShows.new
+        tv_show = api.get action["value"]
+        puts "--> #{tv_show}"
+        {
+          fallback:     "You have started following #{tv_show[:name]}.",
+          color:        "#36a64f", # greenish
+          title:        "You have started following #{tv_show[:name]}.",
+          text:         tv_show[:name],
+        }
+      rescue e
+        Slack::Response::ToYouOnly.error e
+      end
     end
 
-  end  
-end
+    def process_unfollow action
+    end
+  end
+end # module Slack
